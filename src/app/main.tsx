9197d9e6813165f9278b867d75c52b3915ee3526
Ruje Alfon <rujealfon@gmail.com>
2025-05-13 20:28:54 +0800

    Refactor main entry point to use new asset path for styles and add global CSS styles for improved UI consistency.

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,8 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import './index.css'
-import App from './app.tsx'
+import '@/assets/styles/index.css'
+
+import App from './app'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
