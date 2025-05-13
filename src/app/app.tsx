ac74727d0df84144b65396c25efad2c32b81c08f
Ruje Alfon <rujealfon@gmail.com>
2025-05-13 20:21:11 +0800

    Update project structure by adding new app directory, integrating TypeScript, and updating dependencies. Modified entry point in index.html and added styles for the app. Introduced new App component and associated assets.

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,10 +1,10 @@
 import { useState } from 'react'
 
-import reactLogo from './assets/react.svg'
+import reactLogo from '@/assets/images/react.svg'
 
 import viteLogo from '/vite.svg'
 
-import './App.css'
+import '@/assets/styles/app.css'
 
 function App() {
   const [count, setCount] = useState(0)
